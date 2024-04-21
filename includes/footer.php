cc272449988274f861b27f667fda4580880543d2
patelhettt <162837495+patelhettt@users.noreply.github.com>
2024-04-21 09:46:35 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,25 @@
+<div class="footer-section footer-section1">
+<div class="footer-brand">
+    <a href="index.php">
+        <h1 class="footer-heading">Cinemax Cinema</h1>
+    </a> 
+</div>    
+</div>
+<div class="footer-section footer-section2">
+    <h2><i class="fas fa-user-alt"></i> Social Media</h2>
+    <div class="footer-section-inner-container">
+        <span><i class="fab fa-lg fa-facebook-square"></i><a href="https://www.facebook.com/AwesomeHet1/"> Facebook</a></span>
+        <span><i class="fab fa-lg fa-twitter-square"></i><a href="#"> Twitter</a></span>
+        <span><i class="fab fa-lg fa-instagram"></i><a href="https://www.instagram.com/14.hett"> Instagram</a></span>
+    </div>
+</div>
+<div class="footer-section  footer-section3">
+    <p>© 2024 Cinemax Movies.</p>
+    <a>Advertising</a>
+    <a>Privacy Policy</a>
+    <a href="contact-us.php">Contact</a>
+</div>
+
+<footer class="bg-dark d-flex justify-content-center">
+    <h2 style="color:#fff;">Developed By Het Patel and Veer Vora</h2>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<head>
+    <link rel="shortcut icon" type="image/ico" href="/favicon.ico"/>
+</head>
+<div class="navbar-brand">
+    <a href="index.php">
+        <h1 class="navbar-heading">Cinemax Cinema</h1>
+    </a>
+</div>
+<div class="navbar-container">
+    <nav class="navbar">
+        <ul class="navbar-menu">
+            <li><a href="profile.php">Profile</a></li>           
+            <li><a href="signup.php">Login</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="schedule.php">Schedule</a></li>
+            <li><a href="team.php">Team</a></li>           
+            <li><a href="contact-us.php">Contact</a></li>
+        </ul>
+    </nav>
+</div>
